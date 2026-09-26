2b1d4b419dd6dc01020be297ad4e12d2f376a67e
aoi <blueast.923@gmail.com>
2026-09-26 14:10:01 +0900

    Set Firebase config

--- famipedia-test/src/firebaseConfig.ts
+++ famipedia-test/src/firebaseConfig.ts
@@ -4,19 +4,27 @@
 // ファイル（auth.ts）や、他の画面から import して使います。
 // -----------------------------------------------------------
 
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyA-h0_Ao8AjgS0z4uahBpdx2l8Mt3AvW4U",
+  authDomain: "famipedia-58hack.firebaseapp.com",
+  projectId: "famipedia-58hack",
+  storageBucket: "famipedia-58hack.firebasestorage.app",
+  messagingSenderId: "60640939430",
+  appId: "1:60640939430:web:5194ec21b06cf396f3d367",
+  measurementId: "G-GBSH1JFHX5",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
